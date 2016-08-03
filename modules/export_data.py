8c16394483dd57ba1b096a521b533b002a2c4446
David Söderberg <dasovm@gmail.com>
2016-08-03 18:43:55 +0200

    Creates ./stock_data/ if dir doesn't exist

--- modules/export_data.py
+++ modules/export_data.py
@@ -1,3 +1,8 @@
+import os
+
+
 def export_to_csv(df, ticker):
+	if not os.path.exists('./stock_data/'):
+		os.makedirs('./stock_data/')
 	df.to_csv('./stock_data/' + ticker + '.csv')
 	print(str(len(df.index)) + " rows of data has been saved to: " + ticker + '.csv')
