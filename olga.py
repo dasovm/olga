907e632361174eb977204684afebaefc0c029e2f
David Söderberg <dasovm@gmail.com>
2016-08-03 20:13:42 +0200

    Shows how many stocks there still is to download data from

--- olga.py
+++ olga.py
@@ -12,10 +12,16 @@ ticker = 'AOI.ST'
 
 
 def main():
+	stock_count = 0
 	symbols = id.get_symbol_list()
+	for i in symbols.splitlines():
+		stock_count += 1
 	for symbol in symbols.splitlines():
+		print ''
 		df = dr.get_stock_data(symbol + '.ST')
 		ed.export_to_csv(df, symbol + '.ST')
+		stock_count -= 1
+		print(str(stock_count) + ' stocks left.')
 		print '----------'
 
 if __name__ == '__main__':
